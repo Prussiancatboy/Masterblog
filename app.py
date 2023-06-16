5cb5b05c9ca99750d93ddf63b04a266432825e11
Robert <134300632+Prussiancatboy@users.noreply.github.com>
2023-06-16 08:51:44 -0400

    The starting point for features

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+from flask import Flask, render_template
+import json
 
 
-if __name__ == '__main__':
-    app.run()
+def hold():
+    app = Flask(__name__)
+    @app.route('/')
+    def index():
+        with open("data.json") as fileobj:
+            blog_posts = json.load(fileobj)
+        return render_template('index.html', posts=blog_posts[0])
+
+    if __name__ == '__main__':
+        app.run()
+
+hold()
